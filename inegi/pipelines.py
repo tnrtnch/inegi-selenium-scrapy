987822711cc1099d3d4ee65cd2cbdb43b16059c9
tnrtnch <tnrtnch@abv.bg>
2025-11-01 15:32:46 +0200

    Fix pipelines

--- inegi/pipelines.py
+++ inegi/pipelines.py
@@ -1,23 +1,47 @@
-from itemadapter import ItemAdapter
-import json
+# from itemadapter import ItemAdapter
+# import json
+# import os
+
+# class InegiPipeline:
+#     def open_spider(self, spider):
+#         self.data = []
+#         spider.logger.info("Pipeline started (ready to collect data)")
+
+#     def process_item(self, item, spider):
+#         self.data.append(dict(item))
+#         spider.logger.debug(f"Pipeline item received: {item['Entity_name']}")
+#         return item
+
+#     def close_spider(self, spider):
+#         output_file = getattr(spider, "output_file", "sanctions.json")
+#         output_path = os.path.abspath(output_file)
+
+#         with open(output_path, "w", encoding="utf-8") as f:
+#             json.dump(self.data, f, indent=2, ensure_ascii=False)
+
+#         spider.logger.info(f"✅ {len(self.data)} profile(s) saved: {output_path}")
+
+
 import os
+from pathlib import Path
+import json
 
 class InegiPipeline:
     def open_spider(self, spider):
         self.data = []
-        spider.logger.info("Pipeline started (ready to collect data)")
+        spider.logger.info("Pipeline started")
 
     def process_item(self, item, spider):
         self.data.append(dict(item))
-        spider.logger.debug(f"Pipeline item received: {item['Entity_name']}")
         return item
 
     def close_spider(self, spider):
-        output_file = getattr(spider, "output_file", "sanctions.json")
-        output_path = os.path.abspath(output_file)
+        # Proje kök dizinini bul
+        project_root = Path(__file__).resolve().parents[1]
+        output_file = "sanctions.json"
+        output_path = project_root / output_file
 
         with open(output_path, "w", encoding="utf-8") as f:
             json.dump(self.data, f, indent=2, ensure_ascii=False)
 
         spider.logger.info(f"✅ {len(self.data)} profile(s) saved: {output_path}")
-
