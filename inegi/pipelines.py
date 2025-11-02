c965345a49dad027c24066f3558b7a15f3c662ad
tnrtnch <tnrtnch@abv.bg>
2025-11-02 13:04:58 +0200

    Fix pipeline

--- inegi/pipelines.py
+++ inegi/pipelines.py
@@ -29,13 +29,28 @@ class InegiPipeline:
 
     #     spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
 
+    # def close_spider(self, spider):
+    #     output_file = spider.config.get("output_file", "sanctions.json")
+    #     output_path = os.path.abspath(output_file)
+
+    #     output_data = [{"last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S")} + "UTC"] + self.data
+
+    #     with open(output_path, "w", encoding="utf-8") as f:
+    #         json.dump(output_data, f, indent=2, ensure_ascii=False)
+
+    #     spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
+
     def close_spider(self, spider):
-        output_file = spider.config.get("output_file", "sanctions.json")
-        output_path = os.path.abspath(output_file)
+            output_file = spider.config.get("output_file", "sanctions.json")
+            output_path = os.path.abspath(output_file)
 
-        output_data = [{"last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S")} + "UTC"] + self.data
+            # UTC zaman damgası
+            utc_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
 
-        with open(output_path, "w", encoding="utf-8") as f:
-            json.dump(output_data, f, indent=2, ensure_ascii=False)
+            # JSON verisi (liste + tarih başta)
+            output_data = [{"last_updated": utc_time}] + self.data
 
-        spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
+            with open(output_path, "w", encoding="utf-8") as f:
+                json.dump(output_data, f, indent=2, ensure_ascii=False)
+
+            spider.logger.info(f"✅ {len(self.data)} profile(s) saved: {output_path}")
