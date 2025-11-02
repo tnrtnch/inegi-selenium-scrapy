608436371b13564c5514b9c5b4332b98b1f36bc3
tnrtnch <tnrtnch@abv.bg>
2025-11-02 12:41:10 +0200

    Fix yml file

--- inegi/pipelines.py
+++ inegi/pipelines.py
@@ -15,16 +15,28 @@ class InegiPipeline:
         self.data.append(dict(item))
         return item
 
-    def close_spider(self, spider):
-        project_root = Path(__file__).resolve().parents[1]
-        output_path = project_root / "sanctions.json"
+    # def close_spider(self, spider):
+    #     project_root = Path(__file__).resolve().parents[1]
+    #     output_path = project_root / "sanctions.json"
  
-        result = {
-            "last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "records": self.data,
-        }
+    #     result = {
+    #         "last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    #         "":self.data,
+    #     }
+
+    #     with open(output_path, "w", encoding="utf-8") as f:
+    #         json.dump(result, f, indent=2, ensure_ascii=False)
+
+    #     spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
+
+    def close_spider(self, spider):
+        output_file = spider.config.get("output_file", "sanctions.json")
+        output_path = os.path.abspath(output_file)
+
+        # JSON dizisine "last_updated" bilgisi ilk eleman olarak eklenecek
+        output_data = [{"last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] + self.data
 
         with open(output_path, "w", encoding="utf-8") as f:
-            json.dump(result, f, indent=2, ensure_ascii=False)
+            json.dump(output_data, f, indent=2, ensure_ascii=False)
 
         spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
