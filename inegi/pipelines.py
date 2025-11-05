44266b2681592011739c49e7bcbf21bfcb979327
tnrtnch <tnrtnch@abv.bg>
2025-11-05 16:43:06 +0200

    Remove duplicate names and numbers

--- inegi/pipelines.py
+++ inegi/pipelines.py
@@ -13,14 +13,40 @@ class InegiPipeline:
         self.data.append(dict(item)) 
         return item
 
-    def close_spider(self, spider):
-            output_file = spider.config.get("output_file", "sanctions.json")
-            output_path = os.path.abspath(output_file)
+    def close_spider(self, spider):  
+        spider.logger.info("Removing duplicate Entity_name entries...")
+        
+        merged_data = {}
+        for entry in self.data:
+            name = entry["Entity_name"]
+            numbers = entry.get("Sanction_numbers", [])
 
-            utc_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
-            output_data = [{"last_updated": utc_time}] + self.data
+            # Remove duplicate names and numbers
+            if name not in merged_data:
+                    merged_data[name] = set(numbers)
+            else:
+                    merged_data[name].update(numbers)
 
-            with open(output_path, "w", encoding="utf-8") as f:
-                json.dump(output_data, f, indent=2, ensure_ascii=False)
 
-            spider.logger.info(f"{len(self.data)} profile(s) saved: {output_path}")
+        cleaned_data = []
+        for name, numbers in merged_data.items():
+            cleaned_data.append({
+                "Entity_name": name,
+                "Sanction_numbers": sorted(list(numbers))
+            })
+
+        
+        output_file = spider.config.get("output_file", "sanctions.json")
+        output_path = os.path.abspath(output_file)
+
+        utc_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+
+        output_data = {
+            "last_updated": utc_time,
+            "data": cleaned_data,   
+        }
+
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(output_data, f, indent=2, ensure_ascii=False)
+
+        spider.logger.info(f"{len(cleaned_data)} profile(s) saved: {output_path}")
