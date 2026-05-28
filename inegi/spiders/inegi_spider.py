1d4f7443beda68ef27d2407fdb6d84d40b1cb663
tnrtnch <tnrtnch@abv.bg>
2026-05-28 17:32:04 +0300

    fix spider

--- inegi/spiders/inegi_spider.py
+++ inegi/spiders/inegi_spider.py
@@ -25,14 +25,23 @@ class InegiSpider(scrapy.Spider):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
+        options.add_argument("--start-maximized")
 
         self.driver = webdriver.Chrome(options=options)
         self.wait = WebDriverWait(self.driver, 15)
 
     def start_requests(self):
-        yield scrapy.Request(url=self.config["target_url"], callback=self.parse_with_selenium)
+        yield scrapy.Request(
+            url=self.config["target_url"], 
+            callback=self.parse_with_selenium,
+                headers={
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept": "text/html,application/xhtml+xml",
+            }
+    )
 
     def parse_with_selenium(self, response):
         self.driver.get(response.url)
@@ -49,33 +58,87 @@ class InegiSpider(scrapy.Spider):
         yield from self.parse_page()
 
     def parse_page(self):
+        profile_xpath = "(//tbody)[5]//a[contains(@id,'lnkSancionar')]"
+
         while True:
-            all_profiles = self.driver.find_elements(By.XPATH, "(//tbody)[5]//a[contains(@id,'lnkSancionar')]")
-            for i in range(len(all_profiles)):
-                all_profiles = self.driver.find_elements(By.XPATH, "(//tbody)[5]//a[contains(@id,'lnkSancionar')]")
-                all_profiles[i].click()
 
-                name_part = self.wait.until(
-                    EC.presence_of_element_located((By.XPATH, "//caption//span[@class='Etiqueta']"))
-                ).text
-                name = re.split(r'\bproveedor \b', name_part)[-1].strip()
+            profiles_count = len(
+                self.driver.find_elements(By.XPATH, profile_xpath)
+            )
 
-                numbers = [
-                    n.text.strip()
-                    for n in self.driver.find_elements(By.XPATH, "//tr[@style='background-color:#EFEFEF;']/td[2]")
-                    if n.text.strip()
-                ]
+            for i in range(profiles_count):
+                try:
+                    profiles = self.driver.find_elements(By.XPATH, profile_xpath)
 
-                item = InegiItem()
-                item["Entity_name"] = name
-                item["Sanction_numbers"] = numbers
-                yield item
+                    self.driver.execute_script(
+                        "arguments[0].scrollIntoView(true);",
+                        profiles[i]
+                    )
 
-                self.driver.back()
-                self.wait.until(EC.presence_of_all_elements_located(
-                    (By.XPATH, "(//tbody)[5]//a[contains(@id,'lnkSancionar')]")
-                ))
+                    self.wait.until(
+                        EC.element_to_be_clickable((By.XPATH, profile_xpath))
+                    )
+
+                    profiles[i].click()
+
+                    name_part = self.wait.until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, "//caption//span[@class='Etiqueta']")
+                        )
+                    ).text
+
+                    name = re.split(
+                        r'\bproveedor \b',
+                        name_part
+                    )[-1].strip()
+
+                    numbers = []
+
+                    elements = self.driver.find_elements(
+                        By.XPATH,
+                        "//tr[@style='background-color:#EFEFEF;']/td[2]"
+                    )
+
+                    for el in elements:
+                        try:
+                            text = el.text.strip()
+
+                            if text:
+                                numbers.append(text)
+
+                        except Exception as e:
+                            self.logger.warning(f"text read failed: {e}")
+
+                    item = InegiItem()
+                    item["Entity_name"] = name
+                    item["Sanction_numbers"] = numbers
+
+                    yield item
+
+                except Exception as e:
+                    self.logger.error(f"profile failed: {e}")
+
+                    self.driver.save_screenshot(
+                        f"error_{i}.png"
+                    )
+
+                finally:
+
+                    try:
+                        self.driver.back()
+
+                        self.wait.until(
+                            EC.presence_of_all_elements_located(
+                                (By.XPATH, profile_xpath)
+                            )
+                        )
+
+                    except Exception as e:
+                        self.logger.error(f"back failed: {e}")
+                        return
+
             break
+                    
 
     def closed(self, reason):
         if hasattr(self, "driver") and self.driver:
